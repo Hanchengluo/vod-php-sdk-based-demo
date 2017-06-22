c9847571050ca82346a4ff6ff1094d340d01f690
chuxiongyan <chuxiongyan@tencent.com>
2017-06-22 16:20:38 +0800

    删除暴露的密钥

--- upload_demo.php
+++ upload_demo.php
@@ -1,8 +1,6 @@
 <?php
 require_once './vod_api.php';
 
-// 设置secret id 和 secret key
-// Vodapi::setSecretIdAndKey('AKIDmW5UQRaAzmRvJZsrno14BRpAQVe1Io9V', 'Ur69B4mKi3ED2snfl9PetdDevCIEzcHl');
 // 上传视频
 Vodapi::upload('./Wildlife.wmv');
 // 上传视频和封面
